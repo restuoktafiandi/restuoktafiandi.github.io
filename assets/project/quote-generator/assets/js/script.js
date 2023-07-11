beb479e370ae71b581c9f015a407a05fd78ebdb1
Restu Oktafiandi <restuoktafiandi1@gmail.com>
2023-07-11 13:02:06 +0700

    updateProject

---
+++ assets/project/quote-generator/assets/js/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function hideLoadingSpinner() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+function newQuote() {
+  showLoadingSpinner();
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote')
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
+  quoteText.textContent = quote.text;
+  hideLoadingSpinner();
+}
+
+async function getQuotes() {
+  showLoadingSpinner();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+    hideLoadingSpinner();
+  } catch (error) {}
+}
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+
+getQuotes();
